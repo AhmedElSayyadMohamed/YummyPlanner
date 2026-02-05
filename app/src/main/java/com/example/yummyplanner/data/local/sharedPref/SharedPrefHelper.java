8cab943d8eccd5cac78f8a2265b88866a1f65649
Ahmed_El_Sayyad <elsayyada197@gmail.com>
2026-02-05 22:16:23 +0200

    Implement Shared prefKeys for constant keys and impl shared prefHelper for saving onboardigSeen and GuestMode state

--- app/src/main/java/com/example/yummyplanner/data/local/sharedPref/SharedPrefHelper.java
+++ app/src/main/java/com/example/yummyplanner/data/local/sharedPref/SharedPrefHelper.java
@@ -1,4 +1,33 @@
 package com.example.yummyplanner.data.local.sharedPref;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class SharedPrefHelper {
+
+    private final SharedPreferences prefs;
+
+    public SharedPrefHelper (Context context){
+        prefs = context.getSharedPreferences(PrefKeys.PREF_NAME,Context.MODE_PRIVATE);
+    }
+
+    public void setOnboardingSeen(boolean isSeen){
+        prefs.edit().putBoolean(PrefKeys.KEY_ONBOARDING_SEEN,isSeen).apply();
+    }
+
+    public boolean isOnboardingSeen(){
+        return prefs.getBoolean(PrefKeys.KEY_ONBOARDING_SEEN,false);
+    }
+
+    public void setGeust(boolean isGeust){
+        prefs.edit().putBoolean(PrefKeys.KEY_IS_GEUST,isGeust).apply();
+    }
+
+    public boolean isGeust(){
+        return prefs.getBoolean(PrefKeys.KEY_IS_GEUST,false);
+    }
+
+    public void clearSession(){
+        prefs.edit().remove(PrefKeys.PREF_NAME).apply();
+    }
 }
