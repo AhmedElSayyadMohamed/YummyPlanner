4c5b20d4f54d8dc63e6b353d41c24dc33274d7c6
Ahmed_El_Sayyad <elsayyada197@gmail.com>
2026-02-05 22:43:40 +0200

    impl repository for onboardingaSeen And Geust Mode

--- app/src/main/java/com/example/yummyplanner/data/repository/MealRepository.java
+++ app/src/main/java/com/example/yummyplanner/data/repository/MealRepository.java
@@ -1,4 +1,11 @@
 package com.example.yummyplanner.data.repository;
 
 public interface MealRepository {
+    boolean shouldShowOnboarding();
+
+    void setOnboardingCompleted();
+
+    boolean isGuestUser();
+
+    void loginAsGuest();
 }
--- app/src/main/java/com/example/yummyplanner/data/repository/MealRepositoryImpl.java
+++ app/src/main/java/com/example/yummyplanner/data/repository/MealRepositoryImpl.java
@@ -1,4 +1,33 @@
 package com.example.yummyplanner.data.repository;
 
-public class MealRepositoryImpl {
+import com.example.yummyplanner.data.local.MealLocalDataSource;
+
+public class MealRepositoryImpl implements MealRepository {
+
+    private final MealLocalDataSource mealLocalDataSource;
+
+    public MealRepositoryImpl(MealLocalDataSource mealLocalDataSource) {
+        this.mealLocalDataSource = mealLocalDataSource;
+    }
+
+
+    @Override
+    public boolean shouldShowOnboarding() {
+        return mealLocalDataSource.hasSeenOnboarding();
+    }
+
+    @Override
+    public void setOnboardingCompleted() {
+         mealLocalDataSource.saveOnboardingSeen();
+    }
+
+    @Override
+    public boolean isGuestUser() {
+        return mealLocalDataSource.isGeust();
+    }
+
+    @Override
+    public void loginAsGuest() {
+        mealLocalDataSource.saveGeustMode();
+    }
 }
