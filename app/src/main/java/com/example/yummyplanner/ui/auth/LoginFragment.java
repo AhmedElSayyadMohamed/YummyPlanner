c2a707c17a496ddabe00f2bb1593cb664ec119e5
Ahmed_El_Sayyad <elsayyada197@gmail.com>
2026-02-05 08:21:08 +0200

    complete signUp UI with Material3 components and theming

--- app/src/main/java/com/example/yummyplanner/ui/auth/LoginFragment.java
+++ app/src/main/java/com/example/yummyplanner/ui/auth/LoginFragment.java
@@ -3,21 +3,32 @@ package com.example.yummyplanner.ui.auth;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.yummyplanner.R;
+import com.example.yummyplanner.databinding.FragmentLoginBinding;
 
 
 public class LoginFragment extends Fragment {
 
-
+   FragmentLoginBinding loginBinding ;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        loginBinding = FragmentLoginBinding.inflate(inflater,container,false);
+
+        loginBinding.createNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_login_to_signup);
+            }
+        });
+        return loginBinding.getRoot();
     }
+
 }
